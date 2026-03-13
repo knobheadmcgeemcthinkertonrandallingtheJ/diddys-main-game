492b6886463ca91c3302435b43ffac4751e07b92
knobheadmcgeemcthinkertonrandallingtheJ <kai.dignam612@education.nsw.gov.au>
2026-03-13 15:09:14 +1100

    iwelhe
    main commotit

---
+++ bigseaninthe'jung/bigseaninthe_jung.py
@@ -0,0 +1,7 @@
+def main():
+    while True:
+        print("You are a big egghead in the jugnle")
+        print("Option 1: Do something")
+        print("Option 2: Go somewhere")
+        print("Option 3: Do ")
+main()
